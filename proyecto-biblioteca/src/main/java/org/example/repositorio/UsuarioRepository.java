4954f4f057494e8ea17566d6fa017828d187dc0a
emilianomassolin <e.massolin@alumno.um.edu.ar>
2025-05-13 20:50:32 -0300

    creo interfaces de repositorio

---
+++ proyecto-biblioteca/src/main/java/org/example/repositorio/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package org.example.repositorio;
+
+import org.example.modelo.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioRepository {
+    Usuario save(Usuario usuario);
+    Optional<Usuario> findById(Long id);
+    Optional<Usuario> findByEmail(String email);
+    List<Usuario> findAll();
+    void deleteById(Long id);
+    boolean existsById(Long id);
+}
